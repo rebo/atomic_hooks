bd4a4f0e5c7d0a38c75ef97ff1b678742b528bbb
Benjamin Mapp <rebo@MacBook-Pro.local>
2020-08-24 12:53:40 +0100

    working test for atoms and reactions

--- macro/tests/progress.rs
+++ macro/tests/progress.rs
@@ -1,7 +1,7 @@
 #[test]
 fn tests() {
     let t = trybuild::TestCases::new();
-    t.pass("tests/01-parse-header.rs");
+    t.pass("tests/01-basic_atom_reaction.rs");
     //t.pass("tests/02-parse-body.rs");
     //t.compile_fail("tests/03-expand-four-errors.rs");
     //t.pass("tests/04-paste-ident.rs");
