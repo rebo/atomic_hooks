9bac53552d4b0d53683651c3338e7a3e4a4517ba
arn-the-long-beard <arn.menant@gmail.com>
2020-08-31 11:34:14 +0200

    style: removed useless import in observable.rs

--- src/observable.rs
+++ src/observable.rs
@@ -1,12 +1,3 @@
-use crate::reactive_state_access::state_access::{CloneState, StateAccess};
-
-use std::cell::RefCell;
-
-use crate::{
-    reactive_state_functions::{clone_reactive_state_with_id, read_reactive_state_with_id, STORE},
-    store::ReactiveContext,
-};
-
 pub trait Observable<T>
 where
     T: 'static,
